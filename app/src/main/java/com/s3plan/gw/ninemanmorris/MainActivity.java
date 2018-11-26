5476a0686054da963ad3d1d4e95f31313ccd4970
nordpool <beelzenord@gmail.com>
2018-11-26 13:12:05 +0100

    Every place holder on the board is droppable, full utility is still yet to be realized

--- app/src/main/java/com/s3plan/gw/ninemanmorris/MainActivity.java
+++ app/src/main/java/com/s3plan/gw/ninemanmorris/MainActivity.java
@@ -1,5 +1,6 @@
 package com.s3plan.gw.ninemanmorris;
 
+import android.annotation.SuppressLint;
 import android.content.ClipData;
 import android.content.ClipDescription;
 import android.graphics.Color;
@@ -22,6 +23,8 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.s3plan.gw.ninemanmorris.Model.GameState.GameHandler;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnDragListener{
     private GameHandler gameHandler;
@@ -87,12 +90,172 @@ public class MainActivity extends AppCompatActivity implements View.OnDragListen
       //  imageView.setOnDragListener(this);
        // imageView.setOnDragListener(myDragListener);
         initCheckers();
+        initPlaceHolders();
         //pinkButton.setOnDragListener(myDragListener);
       // pinkButton.setOnDragListener(myDragListener);
         //linearLayout.setOnDragListener(myDragListener);
        // pinkButton.setOnTouchListener(myTouchListener);
 
 
+
+
+
+    }
+
+
+
+
+
+    @SuppressLint("ResourceType")
+    private void initPlaceHolders() {
+        View[] imageViews = new View[25];
+
+
+        imageViews[3] = findViewById(R.id.outerMostTopLeft);
+        imageViews[3].setId(3);
+        //imageViews[3].setTag(2,"HOLDER_03");
+        imageViews[3].setOnDragListener(this);
+
+
+        imageViews[6] = findViewById(R.id.outerMostTopCenter);
+        imageViews[6].setId(6);
+       // imageViews[6].setTag(6,"HOLDER_06");
+        imageViews[6].setOnDragListener(this);
+
+
+        imageViews[9] = findViewById(R.id.outerMostTopRight);
+        imageViews[9].setId(9);
+       // imageViews[9].setTag(9,"HOLDER_09");
+        imageViews[9].setOnDragListener(this);
+
+
+        imageViews[2] = findViewById(R.id.middleTopLeft);
+        imageViews[2].setId(2);
+       // imageViews[9].setTag(9,"HOLDER_09");
+        imageViews[2].setOnDragListener(this);
+
+        imageViews[5] = findViewById(R.id.middleMostTopCenter);
+        imageViews[5].setId(5);
+        // imageViews[9].setTag(9,"HOLDER_09");
+        imageViews[5].setOnDragListener(this);
+
+        imageViews[8] = findViewById(R.id.middleMostTopRight);
+        imageViews[8].setId(8);
+        // imageViews[9].setTag(9,"HOLDER_09");
+        imageViews[8].setOnDragListener(this);
+
+
+        imageViews[1] = findViewById(R.id.innerTopLeft);
+        imageViews[1].setId(1);
+        // imageViews[9].setTag(9,"HOLDER_09");
+        imageViews[1].setOnDragListener(this);
+
+        imageViews[4] = findViewById(R.id.innerMostTopCenter);
+        imageViews[4].setId(4);
+        // imageViews[9].setTag(9,"HOLDER_09");
+        imageViews[4].setOnDragListener(this);
+
+
+        imageViews[7] = findViewById(R.id.innerMostTopRight);
+        imageViews[7].setId(7);
+        // imageViews[9].setTag(9,"HOLDER_09");
+        imageViews[7].setOnDragListener(this);
+
+
+        imageViews[24] = findViewById(R.id.middleLeftBall);
+        imageViews[24].setId(24);
+        // imageViews[9].setTag(9,"HOLDER_09");
+        imageViews[24].setOnDragListener(this);
+
+        imageViews[23] = findViewById(R.id.middleLeftmiddle);
+        imageViews[23].setId(23);
+        // imageViews[9].setTag(9,"HOLDER_09");
+        imageViews[23].setOnDragListener(this);
+
+        imageViews[22] = findViewById(R.id.innerLeftmiddle);
+        imageViews[22].setId(22);
+        // imageViews[9].setTag(9,"HOLDER_09");
+        imageViews[22].setOnDragListener(this);
+
+        imageViews[10] = findViewById(R.id.innerRightmiddleBall);
+        imageViews[10].setId(10);
+        // imageViews[9].setTag(9,"HOLDER_09");
+        imageViews[10].setOnDragListener(this);
+
+        imageViews[11] = findViewById(R.id.middleRightmiddleBall);
+        imageViews[11].setId(11);
+        // imageViews[9].setTag(9,"HOLDER_09");
+        imageViews[11].setOnDragListener(this);
+
+        imageViews[12] = findViewById(R.id.middleRightBall);
+        imageViews[12].setId(12);
+        // imageViews[9].setTag(9,"HOLDER_09");
+        imageViews[12].setOnDragListener(this);
+
+        imageViews[19] = findViewById(R.id.bottomLeftBallInner);
+        imageViews[19].setId(19);
+        // imageViews[9].setTag(9,"HOLDER_09");
+        imageViews[19].setOnDragListener(this);
+
+
+        imageViews[16] = findViewById(R.id.bottomCenterBallInner);
+        imageViews[16].setId(16);
+        // imageViews[9].setTag(9,"HOLDER_09");
+        imageViews[16].setOnDragListener(this);
+
+        imageViews[13] = findViewById(R.id.bottomRightBallInner);
+        imageViews[13].setId(13);
+        // imageViews[9].setTag(9,"HOLDER_09");
+        imageViews[13].setOnDragListener(this);
+
+        imageViews[20] = findViewById(R.id.bottomLeftBallMiddle);
+        imageViews[20].setId(20);
+        // imageViews[9].setTag(9,"HOLDER_09");
+        imageViews[20].setOnDragListener(this);
+
+
+        imageViews[17] = findViewById(R.id.bottomCenterBallMiddle);
+        imageViews[17].setId(17);
+        // imageViews[9].setTag(9,"HOLDER_09");
+        imageViews[17].setOnDragListener(this);
+
+        imageViews[14] = findViewById(R.id.bottomRightBallMiddle);
+        imageViews[14].setId(14);
+        // imageViews[9].setTag(9,"HOLDER_09");
+        imageViews[14].setOnDragListener(this);
+
+
+        imageViews[21] = findViewById(R.id.bottomLeftBallOuter);
+        imageViews[21].setId(21);
+        // imageViews[9].setTag(9,"HOLDER_09");
+        imageViews[21].setOnDragListener(this);
+
+        imageViews[18] = findViewById(R.id.bottomCenterBallOuter);
+        imageViews[18].setId(18);
+        // imageViews[9].setTag(9,"HOLDER_09");
+        imageViews[18].setOnDragListener(this);
+
+        imageViews[15] = findViewById(R.id.bottomRightBallOuter);
+        imageViews[15].setId(15);
+        // imageViews[9].setTag(9,"HOLDER_09");
+        imageViews[15].setOnDragListener(this);
+
+
+
+
+
+        /*
+         * The game board positions
+         *
+         * 03           06           09
+         *     02       05       08
+         *         01   04   07
+         * 24  23  22        10  11  12
+         *         19   16   13
+         *     20       17       14
+         * 21           18           15
+         *
+         */
 
 
 
@@ -130,6 +293,8 @@ public class MainActivity extends AppCompatActivity implements View.OnDragListen
             }
             linearLayout.addView(row);
         }
+
+
     }
 
     @Override
@@ -165,6 +330,7 @@ public class MainActivity extends AppCompatActivity implements View.OnDragListen
             case DragEvent.ACTION_DRAG_STARTED:
 
                 // Determines if this View can accept the dragged data
+                v.getId();
                 /**
                  * Get A tag to Show whether or not we should accept this
                  */
@@ -285,13 +451,13 @@ public class MainActivity extends AppCompatActivity implements View.OnDragListen
 
 
                 // Does a getResult(), and displays what happened.
-                if (event.getResult()) {
-                    Toast.makeText(this, "The drop was handled.", Toast.LENGTH_LONG).show();
+               /* if (event.getResult()) {
+                    Toast.makeText(this, "The drop was handled.", Toast.LENGTH_SHORT).show();
 
                 } else {
                     Toast.makeText(this, "The drop didn't work.", Toast.LENGTH_LONG).show();
 
-                }
+                }*/
 
                 // returns true; the value is ignored.
                 return true;
